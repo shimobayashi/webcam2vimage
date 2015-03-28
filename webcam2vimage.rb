ba66cf713c8ecc7fb21d94353bef9c942f7c1b51
shimobayashi <akimasa@shimobayashi.name>
2015-03-29 00:42:18 +0900

    fix

--- webcam2vimage.rb
+++ webcam2vimage.rb
@@ -4,13 +4,13 @@ require 'net/http'
 require 'uri'
 require 'base64'
 
-jpeg = `fswebcam --skip 10 --resolution 640x480 --jpeg 50 --greyscale --read -`
+jpeg = `fswebcam --skip 10 --resolution 640x480 --jpeg -1 --greyscale -`
 exit -1 unless jpeg
 
 res = Net::HTTP.post_form(
   URI.parse('http://vimage.herokuapp.com/images/new'),
   {
-    title: "#{illust.member_name} - #{illust.title}",
+    title: Time.now.to_s,
     url: nil,
     tags: ['webcam2vimage'].join(' '),
     base64: Base64::encode64(jpeg),
